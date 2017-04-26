d6f1e66c8a932a9d895d50dbfe05bd3b62909081
mkettani <mehdikettani89@gmail.com>
2017-04-26 14:09:55 -0400

    Update technician_add.php

--- technician_manager/technician_add.php
+++ technician_manager/technician_add.php
@@ -1,1 +1,10 @@
+<?php include '../view/header.php'; ?>
+<div id="main">
+    <h1>Add Technician</h1>
+    <form action="" method="post" id="aligned">
+        <input type="hidden" name="action" value="add_technician" />
 
+        <label>First Name:</label>
+        <input type="input" name="first_name" />
+        <br />
+      
