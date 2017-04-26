c85a6f74536a22c44be52b58f03e4e2a60f93298
mkettani <mehdikettani89@gmail.com>
2017-04-26 13:39:57 -0400

    Create product_add.php

---
+++ product_manager/product_add.php
@@ -0,0 +1,7 @@
+<?php include '../view/header.php'; ?>
+<div id="main">
+    <h1>Add Product</h1>
+    <form action="" method="post" id="aligned">
+        <input type="hidden" name="action" value="add_product" />
+
+       
