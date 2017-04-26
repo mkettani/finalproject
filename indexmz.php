f15b7bb7e7a424219dcb60f40ff38fca0ebb59ba
mkettani <mehdikettani89@gmail.com>
2017-04-26 18:18:05 -0400

    Update indexmz.php

--- indexmz.php
+++ indexmz.php
@@ -1,1 +1,22 @@
+<?php include 'view/header.php'; ?>
+<div id="main">
+    <h2>Administrators</h2>
+    <ul class="nav">
+        <li><a href="product_manager">Manage Products</a></li>
+        <li><a href="technician_manager">Manage Technicians</a></li>
+        <li><a href="customer_manager">Manage Customers</a></li>
+        <li><a href="incident_create">Create Incident</a></li>
+        <li><a href="under_construction.php">Assign Incident</a></li>
+        <li><a href="under_construction.php">Display Incidents</a></li>
+    </ul>
+    <h2>Technicians</h2>
+    <ul class="nav">
+        <li><a href="under_construction.php">Update Incident</a></li>
+    </ul>
+    <h2>Customers</h2>
+    <ul class="nav">
+        <li><a href="product_register">Register Product</a></li>
+    </ul>
 
+</div>
+<?php include 'view/footer.php'; ?>
