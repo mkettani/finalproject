dab975bba32f3e61f69e3241da593135498cb978
mkettani <mehdikettani89@gmail.com>
2017-04-26 13:45:18 -0400

    Create product_list.php

---
+++ product_manager/product_list.php
@@ -0,0 +1,15 @@
+<?php include '../view/header.php'; ?>
+<div id="main">
+
+    <h1>Product List</h1>
+
+    <div id="content">
+        <!-- display a table of products -->
+        <table>
+            <tr>
+                <th>Code</th>
+                <th>Name</th>
+                <th>Version</th>
+                <th>Release Date</th>
+                <th>&nbsp;</th>
+            </tr>
