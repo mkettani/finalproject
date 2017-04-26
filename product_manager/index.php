a68a5719e801761d888e10d5a858af3b6a945809
mkettani <mehdikettani89@gmail.com>
2017-04-26 17:53:02 -0400

    Update index.php

--- product_manager/index.php
+++ product_manager/index.php
@@ -1,16 +1,36 @@
 <?php
 require('../model/database.php');
-// require('../model/product_db.php');
-
-$action = filter_input(INPUT_POST, 'action');
-if ($action === NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-    if ($action === NULL) {
-        $action = 'under_construction';
-    }
+require('../model/product_db.php');
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+} else if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'list_products';
 }
-
-if ($action == 'under_construction') {
-    include('../under_construction.php');
+if ($action == 'list_products') {
+    // Get product data
+    $products = get_products();
+    // Display the product list
+    include('product_list.php');
+} else if ($action == 'delete_product') {
+    $product_code = $_POST['product_code'];
+    delete_product($product_code);
+    header("Location: .");
+} else if ($action == 'show_add_form') {
+    include('product_add.php');
+} else if ($action == 'add_product') {
+    $code = $_POST['code'];
+    $name = $_POST['name'];
+    $version = $_POST['version'];
+    $release_date = $_POST['release_date'];
+    // Validate the inputs
+    if (empty($code) || empty($name) || empty($version) || empty($release_date)) {
+        $error = "Invalid product data. Check all fields and try again.";
+        include('../errors/error.php');
+    } else {
+        add_product($code, $name, $version, $release_date);
+        header("Location: .");
+    }
 }
 ?>
