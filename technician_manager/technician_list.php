23ab9593b72a0f7600ef35ec51310466d6dc9459
mkettani <mehdikettani89@gmail.com>
2017-04-26 14:06:27 -0400

    Update technician_list.php

--- technician_manager/technician_list.php
+++ technician_manager/technician_list.php
@@ -1,1 +1,9 @@
+<?php include '../view/header.php'; ?>
+<div id="main">
 
+    <h1>Technician List</h1>
+
+    <div id="content">
+        <!-- display a table of technicians -->
+        <table>
+            <tr>
