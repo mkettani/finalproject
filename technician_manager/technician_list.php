6aa61a6465638f829a4d0606a436ab4069913cf7
mkettani <mehdikettani89@gmail.com>
2017-04-26 14:07:14 -0400

    Update technician_list.php

--- technician_manager/technician_list.php
+++ technician_manager/technician_list.php
@@ -7,3 +7,29 @@
         <!-- display a table of technicians -->
         <table>
             <tr>
+                 <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Password</th>
+                <th>&nbsp;</th>
+            </tr>
+            <?php foreach ($technicians as $technician) : ?>
+            <tr>
+                <td><?php echo $technician['firstName']; ?></td>
+                <td><?php echo $technician['lastName']; ?></td>
+                <td><?php echo $technician['email']; ?></td>
+                <td><?php echo $technician['phone']; ?></td>
+                <td><?php echo $technician['password']; ?></td>
+                <td><form action="." method="post">
+                    <input type="hidden" name="action"
+                           value="delete_technician" />
+                    <input type="hidden" name="technician_id"
+                           value="<?php echo $technician['techID']; ?>" />
+                    <input type="submit" value="Delete" />
+                </form></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <p><a href="?action=show_add_form">Add Technician</a></p>
+    </div>
