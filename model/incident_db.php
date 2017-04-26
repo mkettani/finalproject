939a71b224ff388d8c613ef9873c34400b0514b5
mkettani <mehdikettani89@gmail.com>
2017-04-26 18:50:08 -0400

    Update incident_db.php

--- model/incident_db.php
+++ model/incident_db.php
@@ -1,1 +1,13 @@
-
+<?php
+function add_incident($customer_id, $product_code, $title, $description) {
+    global $db;
+    $date_opened = date('Y-m-d');  // get current date in yyyy-mm-dd format
+    $query =
+        "INSERT INTO incidents
+            (customerID, productCode, dateOpened, title, description)
+        VALUES (
+               '$customer_id', '$product_code', '$date_opened',
+               '$title', '$description')";
+    $db->exec($query);
+}
+?>
