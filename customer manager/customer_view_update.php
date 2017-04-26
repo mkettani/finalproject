74a23d6d73493b4bd643768159e36690923c77c0
mkettani <mehdikettani89@gmail.com>
2017-04-26 14:32:50 -0400

    Update customer_view_update.php

--- customer manager/customer_view_update.php
+++ customer manager/customer_view_update.php
@@ -24,3 +24,17 @@
                 </tr>
                 <?php foreach ($customers as $customer) : ?>
                 <tr>
+<td><?php echo $customer['firstName'] . ' ' . $customer['lastName']; ?></td>
+                    <td><?php echo $customer['email']; ?></td>
+                    <td><?php echo $customer['city']; ?></td>
+                    <td><form action="." method="post">
+                        <input type="hidden" name="action" value="display_customer" />
+                        <input type="hidden" name="customer_id"
+                               value="<?php echo $customer['customerID']; ?>" />
+                        <input type="submit" value="Select" />
+                    </form></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        <?php endif; ?>
+    </div>
