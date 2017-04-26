752565415c14b8d7f91ee82b54fcfc0d641671e9
mkettani <mehdikettani89@gmail.com>
2017-04-26 14:28:50 -0400

    Update customer_display.php

--- customer manager/customer_display.php
+++ customer manager/customer_display.php
@@ -1,3 +1,9 @@
 <?php include '../view/header.php'; ?>
 <div id="main">
+  <div id="content">
+        <!-- display a table of customer information -->
+        <h2>View/Update Customer</h2>
+        <form action="" method="post" id="aligned">
+            <input type="hidden" name="action" value="update_customer" />
+            <input type="hidden" name="customer_id" value="<?php echo $customer['customerID']; ?>" />
 
