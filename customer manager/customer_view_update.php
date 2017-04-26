39a77befc76d48615231c934905c3f99363fe960
mkettani <mehdikettani89@gmail.com>
2017-04-26 14:32:01 -0400

    Update customer_view_update.php

--- customer manager/customer_view_update.php
+++ customer manager/customer_view_update.php
@@ -1,1 +1,13 @@
 
+<?php include '../view/header.php'; ?>
+<div id="main">
+
+    <h2>Customer Search</h2>
+    <div id="content">
+        <!-- display a search form -->
+        <form action="index.php" method="post">
+            <input type="hidden" name="action" value="display_customers" />
+            <label>Last Name:</label>
+            <input type="input" name="last_name" value="<?php echo $last_name; ?>" />
+            <input type="submit" value="Search" />
+        </form>
