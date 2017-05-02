7139eb770c03608d4b502ac697f5672e9bc711e9
mkettani <mehdikettani89@gmail.com>
2017-05-02 16:52:28 -0400

    Update database_error.php

--- errors/database_error.php
+++ errors/database_error.php
@@ -1,9 +1,10 @@
 <?php include '../view/header.php'; ?>
-<main>
-    <h1>Database Error</h1>
+<div id="main">
+    <h1 class="top">Database Error</h1>
     <p>There was an error connecting to the database.</p>
     <p>The database must be installed as described in appendix A.</p>
     <p>The database must be running as described in chapter 1.</p>
     <p>Error message: <?php echo $error_message; ?></p>
-</main>
+    <p>&nbsp;</p>
+</div><!-- end main -->
 <?php include '../view/footer.php'; ?>
