6ffe2399198b4edee946c97e20348bc430bb6794
mkettani <mehdikettani89@gmail.com>
2017-05-02 18:54:15 -0400

    Update product_add.php

--- product_manager/product_add.php
+++ product_manager/product_add.php
@@ -1,30 +1,32 @@
-<?php include '../view/header.php'; ?>
-<div id="main">
+<?php include '../../view/header.php'; ?>
+<main>
     <h1>Add Product</h1>
-    <form action="" method="post" id="aligned">
-        <input type="hidden" name="action" value="add_product" />
+    <form action="index.php" method="post" id="add_product_form">
+        <input type="hidden" name="action" value="add_product">
 
         <label>Code:</label>
-        <input type="input" name="code" />
-        <br />
+        <input type="text" name="code" />
+        <br>
 
         <label>Name:</label>
-        <input type="input" name="name" />
-        <br />
+        <input type="text" name="name" />
+        <br>
 
         <label>Version:</label>
-        <input type="input" name="version" />
-        <br />
-
+        <input type="text" name="version" />
+        <br>
+        
         <label>Release Date:</label>
-        <input type="input" name="release_date" />&nbsp;<span>Use 'yyyy-mm-dd' format</span>
-        <br />
+        <input type="text" name="date" id="datepicker" />
+        <br>
 
         <label>&nbsp;</label>
         <input type="submit" value="Add Product" />
-        <br />
+        <br>
     </form>
-    <p><a href="?action=list_products">View Product List</a></p>
+    <p class="last_paragraph">
+        <a href="index.php?action=list_products">View Product List</a>
+    </p>
 
-</div>
-<?php include '../view/footer.php'; ?>
+</main>
+<?php include '../../view/footer.php'; ?>
