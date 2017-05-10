70f63adc5b43931d069e59a040a7539095c5ee13
mkettani <mehdikettani89@gmail.com>
2017-05-10 16:49:54 -0400

    Delete registration_db.php

--- model/registration_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-function add_registration($customer_id, $product_code) {
-    global $db;
-    $date = date('Y-m-d');  // get current date in yyyy-mm-dd format
-    $query = "INSERT INTO registrations VALUES
-            ('$customer_id', '$product_code', '$date')";
-    $db->exec($query);
-}
-?>
